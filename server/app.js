6741f5b419d32d8d467df48d5b477cb20e85e1ae
Samantha Klein <saradurga@gmail.com>
2016-11-19 10:29:38 -0600

    initial

---
+++ server/app.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+
+
+// modules
+// var recipes = require('./routes/recipes')
+// var index = require('./routes/index');
+
+
+// middleware
+app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// express routes
+// app.use('/recipes', recipes);
+// app.use('/', index);
+
+
+// start server
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), function () {
+  console.log('listening on port ', app.get('port'));
+});
